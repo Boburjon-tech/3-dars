ef02cdb7d1a3fa27eb1e09ca939526f88ca8aca1
bobur <bobur@gmail.com>
2025-01-15 15:04:02 +0500

    all ok

---
+++ app.js
@@ -0,0 +1,196 @@
+// // BEGIN-1;
+
+// let a = Number(prompt("'a'ga qiymat bering:"));
+
+// let b = Number(prompt("'b' ga qiymat bering:"));
+
+// let S = a * b;
+// let P = (a+b)*2;
+
+// console.log(`To'gri to'rtburchakning perimetri: ${P}ga teng, yuzasi esa ${S}ga`);
+
+
+// // BEGIN-2;
+
+// let d = Number(prompt("aylanaga diametrini bering:"));
+
+// let L = d * 3.14;
+// let r = d / 2;
+// let s = 3.14 * r**2;
+
+// console.log(`Uning uzunligi ${L} va yuzasi ${s}`);
+
+
+// // BEGIN-3;
+
+// let kub_tomon = Number(prompt("Kubning tomonini kiriting:"));
+
+// let V = kub_tomon**3;
+// let S_ = 6 *kub_tomon**2;
+
+// console.log(` Uning hajmi ${V} va to'la sirti ${S_}`);
+
+// // BEGIN-4;
+
+// let q = Number(prompt("'q'ga qiymat bering:"));  
+
+// let p = Number(prompt("'p'ga qiymat bering:")); 
+
+// let g = Number(prompt("'g'ga qiymat bering:"));
+
+// let V_ = q*p*g;
+
+// let s_ = 2*(q*p+p*g+q*g);
+
+// console.log(`Uning hajmi ${V_} va to'la sirti ${s_}`);
+
+// // BEGIN-5;
+
+// let a_ = Number(prompt("'A'ga qiymat bering:"));
+
+// let b_ = Number(prompt("'b'ga qiymat bering:"));
+
+// let M = (a_+b_) / 2 | 0;
+
+// console.log(`Ularning orta arifmetigi: ${M}`);
+
+// // BEGIN-6;
+
+// let A = Number(prompt("'A'ga qiymat bering:"));
+
+// let B_ = Number(prompt("'B'ga qiymat bering:"));
+
+// let C = Math.sqrt((A**2)+(B_**2));
+
+// let P_ = C + A + B_;
+
+// console.log(`Uning gipotenuzasi ${C} va perimetri ${P_}`);
+
+// // BEGIN-7;
+
+// let r1 = Number(prompt("'r1'ning qiymatini bering:"));
+
+// let r2 = Number(prompt("'r2'ning qiymatini bering:"));
+
+// let S1 = 3.14 * r1;
+
+// let S2 = 3.14 * r2;
+
+// let S___ = 3.14 * (r1**2 - r2**2);
+
+// console.log(`Ularning yuzalari S1: ${S1} va S2: ${S2}, ularning ayirmasi : ${S___}`);
+
+
+// // BEGIN-8;
+
+// let L_ = Number(prompt("'L'ning qiymatini kiriting:"));
+
+// let R_ = L_ / 2*3.14;
+
+// let S__ = 3.14 * R_**2;
+
+// console.log(` Uning radiusi: ${R_} va yuzasi: ${S__} `);
+
+// // BEGIN-9;
+
+// let x1 = Number(prompt("x1ni qiymatini kiriting:"));
+
+// let y1 = Number(prompt("y1ni qiymatini kiriting:"));
+
+// let x2 = Number(prompt("x2ni qiymatini kiriting:"));
+
+// let y2 = Number(prompt("y2ni qiymatini kiriting:"));
+
+
+// let L_all = Math.sqrt(((x1-y1)**2) +  ((x2-y2)**2));
+
+// console.log(`L masofa  ${L_all}`);
+
+// BEGIN-10;
+
+// let a__ = Number(prompt("ani qiymatini kiriting:"));
+
+// let b__ = Number(prompt("bni qiymatini kiriting:"));
+
+// let c__ = Number(prompt("cni qiymatini kiriting:"));
+
+// let h = b__;
+
+// b__ = a__;
+
+// a__ = c__;
+
+// c__ = h;
+
+// console.log(`'a'ning yangi qiymati:${a__}, 'b'ning yangi qiymati:${b__} 'c'ning yangi qiymati:${c__}`);
+
+
+
+// INTEGER-1;
+
+// let three = 123;
+
+// let first = three %100 %10;
+
+// let second = three %100 / 10 | 0;
+
+// console.log(`birlik xonasidagi son: ${first} , onlik xonasidagi son: ${second}`);
+
+
+// Integer-2;
+
+// let three_room = 123;
+
+// let first_ = three_room % 100 % 10;
+
+// let second_ = three_room %100 / 10 | 0;
+
+// let third = three_room / 100 | 0;
+
+// let all_value = first_+second_+third;
+
+// console.log(`uch xonalik sonlarning yig'indisi: ${all_value}`);
+
+
+// INTEGER-3;
+
+// let three_room_ = 123;
+
+// let first__ = three_room_ % 100 % 10;
+
+// let second__ = three_room_ %100 / 10 | 0;
+
+// let third_ = three_room_ / 100 | 0;
+
+// let all_value_ = (first__*100)+(second__*10)+third_;
+
+// console.log(`uchxonalik sonni teskari tartibi: ${all_value_}`);
+
+// INTEGER-4;
+
+// let three_room__ = 123;
+
+// let first___ = three_room__ % 100 % 10;
+
+// let second___ = three_room__ %100 / 10 | 0;
+
+// let third__ = three_room__ / 100 | 0;
+
+// let all_value__ = (second___*100)+(third__*10)+first___;
+
+// console.log(`yuzliklar va o'nliklarning  almashinuvi: ${all_value__}`);
+
+
+let three_room____ = 123;
+
+ let first_____ = three_room____ % 100 % 10;
+
+let second_____ = three_room____ %100 / 10 | 0;
+
+let third____ = three_room____ / 100 | 0;
+
+let all_value____ = (third____*100)+(first_____*10)+second_____;
+
+console.log(`birliklar va o'nliklarning  almashinuvi: ${all_value____}`);
+
+
